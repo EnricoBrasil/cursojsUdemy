b55b0a808315912e1003b584646a01494d37d302
EnricoBrasil <cylodabiliro@hotmail.com>
2023-08-26 02:03:51 -0300

    5 classes

---
+++ aula5/index.js
@@ -0,0 +1,6 @@
+      //comentario aleatorio
+      console.log(`ola galerinha`)
+      alert(`ola galerinha`)
+      console.log(`APENAS UM ROMANTICO`)
+      //NÃO EXISTE alert() no nodejs Apenas no javaScript
+
