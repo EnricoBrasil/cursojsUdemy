e9e526ce6a46b7827b7d6dae9f7657d14339b63d
EnricoBrasil <cylodabiliro@hotmail.com>
2023-08-26 01:35:36 -0300

    classes 3 and 4

---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('enrico');
+console.log("enrico");
+console.log(`enrico é um "lindo" `); //AQUI É POSSIVEL COLOCAR ASPAS TRANQUILAMENTE 
+
+console.log(`Meu nome é "Enrico". Estou aprendendo JavaScript às ${10} da noite`)
