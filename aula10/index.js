905d0502ac2df12cf2407010c9a304ada03ae1cd
EnricoBrasil <cylodabiliro@hotmail.com>
2023-08-30 01:47:58 -0300

    classes 10 arrays

--- aula10/index.js
+++ aula10/index.js
@@ -3,3 +3,8 @@
 const alunos = [5,2,6,1]
 
 console.log();
+
+/*COMANDOS BASICOS PARA O ARRAY
+   nomedoArray.push() = ADICIONA UM NOVO VALOR NO FINAL DO ARRAY 
+   nomedoArray.pop() = exclui o ultimo valor do fim do array
+   nomedoArray.unshift() = coloca um novo valor no indice [0] do array*/
