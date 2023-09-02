17769076999b88b8916594fb0effa003b01b1fdc
EnricoBrasil <cylodabiliro@hotmail.com>
2023-09-01 23:36:29 -0300

    updates

--- aula10/index.js
+++ aula10/index.js
@@ -8,3 +8,20 @@ console.log();
    nomedoArray.push() = ADICIONA UM NOVO VALOR NO FINAL DO ARRAY 
    nomedoArray.pop() = exclui o ultimo valor do fim do array
    nomedoArray.unshift() = coloca um novo valor no indice [0] do array*/
+
+   //METODOS DE ARRAYS
+
+   const nomes = [
+      {nome: "enrico", idade: 22},
+      {nome: "fabio", idade: 32},
+      {nome: "phaedra", idade: 43},
+   ]
+
+   const adicionar = nomes.push({
+      nome:"everson", idade:11
+   })
+
+
+
+   console.log(nomes)
+   console.log(nomes.random())
--- aula11/index.js
+++ aula11/index.js
@@ -4,6 +4,7 @@
 // Math.round() Arredonda de forma automatica
 // Math.max() pegar maior numero de uma lista de numeros 
 // Math.min() pegar menor numero de uma lista de numeros 
+//Math.random() pega um numero aleatorio
 
 //CRIAR NUMEROS ALEATORIOS
 let aleatorio = Math.round(Math.random() * (100 - 5) + 5)
